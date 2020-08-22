10bf2215cd100946247a25d7004c21c67d915120
mohammad <mizan.atms@gmail.com>
2020-08-22 03:35:02 -0400

    30 amazon test cases done

--- src/amazon/testCases/Main.java
+++ src/amazon/testCases/Main.java
@@ -3,109 +3,221 @@ package amazon.testCases;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
+import java.util.concurrent.TimeUnit;
+
 public class Main {
 
-    public WebDriver webDriver;
+    public WebDriver driver;
     public String chromeDriver = "webdriver.chrome.driver";
     public String chromeDriverPath = "lib/browserDriver/chromedriver";
-    public String url="https://www.amazon.com/";
-    public String signInurl = "https://www.amazon.com/ap/signin?openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2F%3Fref_%3Dnav_ya_signin&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.assoc_handle=usflex&openid.mode=checkid_setup&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&";
-    public String emailBox = "//*[@id=\"ap_email\"]";
-    public String emailId = "pnt30242@gmail.com";
-    public String continueButton = "//*[@id=\"continue\"]";
-    public String passwordBox = "//*[@id=\"ap_password\"]";
-    public String password="Pnt@30242";
+    public String url = "https://www.amazon.com/";
     public String signInButton = "//*[@id=\"signInSubmit\"]";
-    public String actualText="//*[@id=\"nav-logo\"]/a/span[1]";
-    public String expectedText="\"Amazon Home Page\"";
+    public String actualText = "//*[@id=\"nav-logo\"]/a/span[1]";
+    public String expectedText = "\"Amazon Home Page\"";
+
+
+    public void setUp1(String driverUrl, String driverPath, String url) {
+        System.setProperty(driverUrl, driverPath);
+        driver = new ChromeDriver();
+        driver.get(url);
+    }
 
     @BeforeMethod
-    public void welcome() {
-        System.out.println("Welcome to Amazon Automation world");
+    public void setUp() {
+        System.out.println("Welcom to Amazon testing page ");
+        System.setProperty("webdriver.chrome.driver", "lib/browserDriver/chromedriver");
+        driver = new ChromeDriver();
+        driver.navigate().to("https://www.amazon.com/");
+        driver.manage().window().maximize();
+        driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
     }
-    public void lounchBrowser(String driverUrl,String driverPath, String signInurlurl){
-        //for chromeBrowser
-        System.setProperty(driverUrl, driverPath);
-        webDriver= new ChromeDriver();
-        webDriver.get(signInurlurl);
-    }
-    public void setUp(String driverUrl,String driverPath, String url) {
-        System.setProperty(driverUrl, driverPath);
-        webDriver= new ChromeDriver();
-        webDriver.get(url);
-    }
-//        @AfterMethod
-//    public void closeBrowser() {
-//        webDriver.close();
- //   }
-/*
-Test Amazon signIn
- */
-//  # Sign in Functionality test
 
-//•	Open Chrome browser
-//•	Enter amazon sign in url in address bar
-//•	Click on Sign button
-//•	Enter valid email id
-//•	Click on Continue Button
-//•	Enter valid password
-//•	Click on Sign in button
-//•	Verify Home page displayed properly
-    public void setlogIn() throws InterruptedException {
-        lounchBrowser(chromeDriver,chromeDriverPath,url);
-        webDriver.findElement(By.xpath(emailBox)).sendKeys(emailId);
-        webDriver.findElement(By.xpath(continueButton)).click();
+    @AfterMethod
+    public void closeBrowser() {
+        driver.close();
+        System.out.println("Amazon Automation Ended");
+    }
+
+    public void logIn() throws InterruptedException {
+        driver.findElement(By.xpath("//span[contains(text(),'Hello, Sign in')]")).click();
+        driver.findElement(By.xpath("//input[@id='ap_email']")).sendKeys("pnt30242@gmail.com");
+        driver.findElement(By.xpath("//input[@id='continue']")).click();
         Thread.sleep(3000);
-        webDriver.findElement(By.xpath(passwordBox)).sendKeys(password);
-        webDriver.findElement(By.xpath(signInButton)).click();
+        driver.findElement(By.xpath("//*[@id=\"ap_password\"]")).sendKeys("Pnt@30242");
+        //driver.findElement(By.xpath(signInButton)).click();
         Thread.sleep(3000);
     }
-
-
-/*
-testing amazon shopping cart
- */
-   public String shoppingCartIcon = "//*[@id=\"nav-cart-count-container\"]/span[2]";
-   public String cart = "Cart";
-    public String cartHomePageActualText = "#sc-active-cart > div > div.a-column.a-span8.a-span-last > div.a-row.sc-your-amazon-cart-is-empty > h2";
-    public String expectedCartText = "\"Your Amazon Cart is empty\"";
 
     public void shoppingCart() throws InterruptedException {
-        System.setProperty(chromeDriver,chromeDriverPath);
-        WebDriver webDriver= new ChromeDriver();
-        webDriver.get(url);
-        webDriver.findElement(By.xpath(shoppingCartIcon)).click();
+        driver.findElement(By.xpath("//*[@id=\"nav-cart-count-container\"]/span[2]")).click();
         Thread.sleep(3000);
-        webDriver.close();
-    }
-        /*
-    Test Try Prime button
-     */
-    //  # Sign in Functionality test
-//•	Open Chrome browser
-//•	Enter amazon home page url in address bar hit enter
-//•	Click on Try Prime Button
-//•	Verify Home page displayed properly
-
-   public String tryPrimeButton="//*[@id=\"nav-link-prime\"]";
-    public String primeActualText="//*[@id=\"prime-hero-header\"]/div[1]/img";
-    public String primeExpectedText="\"Prime\"";
-
-    public void setTryPrimeButton() throws InterruptedException {
-        System.setProperty(chromeDriver,chromeDriverPath);
-        WebDriver webDriver=new ChromeDriver();
-        webDriver.get(url);
-        webDriver.findElement(By.xpath(tryPrimeButton)).click();
-        Thread.sleep(3000);
-        webDriver.findElement(By.xpath(primeActualText)).getText();
-        Thread.sleep(3000);
-        webDriver.close();
     }
 
+    public void tryPrimeButton() throws InterruptedException {
+        driver.findElement(By.xpath("//a[@id='nav-link-prime']//div[@class='nav-line-1-container']")).click();
+        Thread.sleep(3000);
+        System.out.println(driver.getTitle());
+    }
 
+    public String amazonBasicButton = "//*[@id=\"nav-xshop\"]/a[12]";
+    public String getAmazonBasicActualText = "//*[@id=\"Header-DQNwtPU\"]/div/div[1]/img";
+    public String getAmazonBasicExpectedsText = "\"amazonbasic";
 
+    public void amazonBasic() throws InterruptedException {
+        driver.manage().window().fullscreen();
+        driver.findElement(By.xpath(amazonBasicButton)).click();
+    }
+
+    public void sellButton() throws InterruptedException {
+        driver.manage().window().fullscreen();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"nav-xshop\"]/a[10]")).click();
+    }
+
+    public void setProductSearch() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("watch for men");
+        driver.findElement(By.xpath("//*[@id=\"nav-search-submit-text\"]/input")).click();
+        driver.findElement(By.xpath("//span[contains(text(),'Mens Watches Ultra-Thin Minimalist Waterproof - Fa')]")).click();
+        System.out.println(driver.findElement(By.xpath("//span[@id='productTitle']")).isDisplayed());
+    }
+
+    public void setSelectYourAddressd() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"glow-ingress-block\"]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id=\"GLUXSignInButton\"]/span/input")).click();
+    }
+
+    public void amazonBurgerButtonIsDiplayed() {
+        System.out.println(driver.findElement(By.xpath("//i[@class='hm-icon nav-sprite']")).isDisplayed());
+    }
+
+    public void amazonBurgerButtonIsEnable() {
+        System.out.println(driver.findElement(By.xpath("//i[@class='hm-icon nav-sprite']")).isEnabled());
+    }
+
+    public void giftCardButtonDisplay() {
+        driver.findElement(By.xpath("//a[contains(text(),'Gift Cards')]")).isDisplayed();
+    }
+
+    public void giftCardButtonClickable() {
+        driver.findElement(By.xpath("//a[contains(text(),'Gift Cards')]")).click();
+    }
+
+    public void giftCardHomePageDisplay() {
+        driver.findElement(By.xpath("//a[contains(text(),'Gift Cards')]")).click();
+    }
+
+    public void eGiftCardButtonIsClickAble() {
+        giftCardHomePageDisplay();
+        driver.findElement(By.xpath("//div[@id='contentGrid_44434']//div[contains(@class,'bxc-grid__row--light')]//div[1]//div[1]//div[1]//a[1]//img[1]")).click();
+    }
+
+    public void birthDayButtonIsActive() throws InterruptedException {
+        driver.findElement(By.xpath("//a[contains(text(),'Gift Cards')]")).click();
+        driver.findElement(By.xpath("//span[contains(text(),'Birthdays')]")).isDisplayed();
+        Thread.sleep(3000);
+    }
+
+    public void redeemGiftCard() throws InterruptedException {
+        driver.findElement(By.xpath("//a[contains(text(),'Gift Cards')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[contains(text(),'Redeem Gift Cards')]")).click();
+        Thread.sleep(3000);
+    }
+
+    public void viewYourBalace() throws InterruptedException {
+        driver.findElement(By.xpath("//a[contains(text(),'Gift Cards')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[contains(text(),'View Your Balance')]")).click();
+        Thread.sleep(3000);
+    }
+
+    public void reloadYourBalance() throws InterruptedException {
+        driver.findElement(By.xpath("//a[contains(text(),'Gift Cards')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath(" //span[contains(text(),'Reload Your Balance')]")).click();
+    }
+
+    public void byBrand() throws InterruptedException {
+        driver.findElement(By.xpath("//a[contains(text(),'Gift Cards')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[contains(text(),'By Brand')]")).click();
+    }
+
+    public void amazonCash() throws InterruptedException {
+        driver.findElement(By.xpath("//a[contains(text(),'Gift Cards')]")).click();
+        Thread.sleep(3000);
+        driver.findElement(By.xpath("//span[contains(text(),'Amazon Cash')]")).click();
+    }
+
+    public void searchBoxIsTypable() {
+        driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).clear();
+        driver.findElement(By.xpath("//*[@id=\"twotabsearchtextbox\"]")).sendKeys("Samsung s20 plus");
+        driver.findElement(By.xpath("//*[@id=\"nav-search\"]/form/div[2]/div/input")).click();
+    }
+
+    public void landingCuponePage() throws InterruptedException {
+        searchBoxIsTypable();
+        Thread.sleep(5000);
+        driver.findElement(By.xpath("//a[contains(text(),'Coupons')]")).click();
+    }
+
+    public void landingRecentlyClippedPage() throws InterruptedException {
+        landingCuponePage();
+        driver.findElement(By.xpath("//a[contains(text(),'Recently Clipped')]")).click();
+    }
+
+    public void landingMostPopularpage() throws InterruptedException {
+        landingCuponePage();
+        driver.findElement(By.xpath("//a[contains(text(),'Most Popular')]")).click();
+    }
+
+    public void searchCouponBoxIsTypable() throws InterruptedException {
+        landingCuponePage();
+        driver.findElement(By.xpath("//input[@id='csTextInput']")).sendKeys("SAMSUNG");
+        driver.findElement(By.xpath("//span[@id='csSearchButton']//input[@class='a-button-input']")).click();
+    }
+
+    public void landingOnFire7Page() {
+        driver.findElement(By.xpath("//i[@class='hm-icon nav-sprite']")).click();
+        driver.findElement(By.xpath("//div[contains(text(),'Fire Tablets')]")).click();
+        driver.findElement(By.xpath("//ul[@class='hmenu hmenu-visible hmenu-translateX']//li[3]//a[1]")).click();
+    }
+
+    public void bestSellersButton() {
+        driver.findElement(By.xpath("//*[@id=\"nav-xshop\"]/a[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"zg_tabs\"]/ul/li[1]/div/a")).click();
+    }
+
+    public void customerServices() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"nav-xshop\"]/a[2]")).click();
+        driver.findElement(By.xpath("/html/body/div[2]/div[1]/h1")).click();
+        Thread.sleep(2000);
+    }
+
+    public void todaysDeals() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"nav-xshop\"]/a[3]")).click();
+        driver.findElement(By.xpath("//*[@id=\"nav-subnav\"]/a[1]/span")).click();
+        Thread.sleep(200);
+
+    }
+
+    public void newRelease() {
+        driver.findElement(By.xpath("//*[@id=\"nav-xshop\"]/a[4]")).click();
+        driver.findElement(By.xpath("//*[@id=\"zg_banner_text_wrapper\"]")).click();
+
+    }
+
+    public void findAGiftButton() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id=\"nav-xshop\"]/a[5]")).click();
+        driver.findElement(By.xpath("//*[@id=\"nav-subnav\"]/a[1]/span")).click();
+        Thread.sleep(2000);
+    }
 }
